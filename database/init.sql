31d088ecb947f1208450d0290d04d97d68d3d3ea
AntIns01 <antonioinsfranriquelme@fpuna.edu.py>
2022-09-06 00:23:43 -0400

    Estructuras proyecto

---
+++ database/init.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.usuario_permiso(
+	id, descripcion)
+	VALUES 
+    (1, 'Default');
+INSERT INTO public.usuario_rol(
+	id, descripcion)
+	VALUES 
+    (1,'Administrador'),
+    (2, 'Observador');
