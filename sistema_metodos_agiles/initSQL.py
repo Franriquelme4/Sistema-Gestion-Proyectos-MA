08ba64bdde67ace71c71f444363a40155f83ccad
Francisco Riquelme <francisco.riquelme@konecta.com.py>
2022-10-19 21:27:45 -0300

    Actualizacion de sqlInicial

--- sistema_metodos_agiles/initSQL.py
+++ sistema_metodos_agiles/initSQL.py
@@ -6,15 +6,27 @@ import django
 django.setup()
 from usuario.models import Rol,Permiso
 
+def cargarPermiso():
+    permiso = Permiso.objects.get_or_create(
+        id=1,
+        nombre_permiso="def",
+        descripcion_permiso="Default"
+    )[0]
+    permiso.save()
+
+    
+
 def cargarDB():
     """Se carga la base de datos inicial"""
     rol = Rol.objects.get_or_create(
-            id=9000,
+            id=900000,
             descripcion_rol = "Poblacion DB 2",
             nombre_rol = "Poblacion DB 2",
     )[0]
     rol.save()
     rol.permiso.add(Permiso.objects.get(id=1))
+
+
 
 if __name__ == '__main__':
     cargarDB()
